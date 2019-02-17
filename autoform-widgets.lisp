f215e6e44379794bdc3b5b678a436ec161d0e844
Sven Michael Klose <pixel@hugbox.org>
2019-02-17 11:09:01 +0100

    is_editable -> editable?

--- autoform-widgets.lisp
+++ autoform-widgets.lisp
@@ -52,4 +52,4 @@
 
 (fn make-fields-editable (schema &rest fields)
   (@ (i (| fields (property-names schema)) schema)
-    (= (aref schema i).is_editable t)))
+    (= (aref schema i).editable? t)))
