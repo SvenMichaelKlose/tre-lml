f2c2e4e7b63b9dacd178e2f3a115cb0f1769f873
Sven Michael Klose <pixel@hugbox.org>
2019-03-24 10:22:19 +0100

    Have empty strings instead of "null".  Fixed default value for checkboxes.

--- autoform-widgets.lisp
+++ autoform-widgets.lisp
@@ -7,19 +7,26 @@
 
 (def-autoform-widget (store name schema v) [& _.is_editable
                                               (eql _.type "selection")]
-  `(select :key        ,name
-           :on-change  ,[store.write (make-object name ((_.element).get ":checked").value)]
-           ,@(!? schema.is_required `(:required "yes"))
-     ,@(@ [`(option :value ,_
-                    ,@(? (| (eql _ v)
-                            (eql _ schema.default))
-                         `(:selected "yes"))
-              ,(aref schema.options _))]
-          (property-names schema.options))))
+  (with (has-default?  (defined? schema.default)
+         av            (autoform-value schema v))
+    `(select :key        ,name
+             :on-change  ,[store.write (make-object name ((_.element).get ":checked").value)]
+             ,@(!? schema.is_required `(:required "yes"))
+       ,@(@ [`(option :value ,_
+                      ,@(? (eql _ av)
+                           `(:selected "yes"))
+                ,(aref schema.options _))]
+            (property-names schema.options)))))
 
 (fn autoform-pattern-required (schema)
   `(,@(!? schema.pattern      `(:pattern ,!))
     ,@(!? schema.is_required  `(:required "yes"))))
+
+(fn autoform-value (schema v)
+  (| v
+     (& (defined? schema.default)
+        schema.default)
+     ""))
 
 (fn make-autoform-input-element (typ store name schema v)
   `(input :type  ,typ
@@ -27,7 +34,7 @@
           ,@(!? schema.size `(:size ,!))
           ,@(autoform-pattern-required schema)
           :on-change  ,[store.write (make-object name (_.element).value)]
-          :value      ,(| v schema.default "")))
+          :value      ,(autoform-value schema v)))
 
 (def-autoform-widget (store name schema v) [& _.is_editable
                                               (in? _.type "string" "password" "email")]
@@ -38,27 +45,29 @@
 
 (def-autoform-widget (store name schema v) [& _.is_editable
                                               (eql _.type "boolean")]
-  `(input :type      "checkbox"
-          :key       ,name
-          :on-click  ,[store.write (make-object name (not v))]
-          ,@(& (| v schema.default)
-               '(:checked "1"))))
+  (let has-value-in-record? (defined? (slot-value store.data name))
+    `(input :type      "checkbox"
+            :key       ,name
+            :on-click  ,[store.write (make-object name (not v))]
+            ,@(& has-value-in-record?
+                 (not (empty-string? (autoform-value schema v)))
+                 '(:checked "1")))))
 
 (def-autoform-widget (store name schema v) [& _.is_editable
                                               (eql _.type "text")]
   `(textarea :key  ,name
              ,@(autoform-pattern-required schema)
              :on-change  ,[store.write (make-object name (_.element).value)]
-     ,(| v schema.default "")))
+     ,(autoform-value schema v)))
 
 (def-autoform-widget (store name schema v) [eql _.type "text"]
-  `(pre ,(| v schema.default "")))
+  `(pre ,(autoform-value schema v)))
 
 (def-autoform-widget (store name schema v) [eql _.type "selection"]
   (aref schema.options v))
 
 (def-autoform-widget (store name schema v) [identity t]
-  (| v schema.default ""))
+  (autoform-value schema v))
 
 (fn set-schema-items (value what schema &rest fields)
   (@ (i (| fields (property-names schema)) schema)
