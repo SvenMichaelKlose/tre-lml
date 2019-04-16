3be4766b2ebd41af957139ca145e71d78c48b3ac
Sven Michael Klose <pixel@hugbox.org>
2019-04-16 19:48:12 +0200

    DEFINE-EXPANDER-MACRO -> DEF-EXPANDER-MACRO.

--- expand.lisp
+++ expand.lisp
@@ -2,7 +2,7 @@
 
 (defmacro define-lml-macro (&rest x)
   (print-definition `(define-lml-macro ,x. ,.x.))
-  `(define-expander-macro *lml-expander* ,@x))
+  `(def-expander-macro *lml-expander* ,@x))
 
 (fn lml-expand (x)
   (expander-expand *lml-expander* x))
